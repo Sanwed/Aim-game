920befb6bc26202a066c8385680dbd0746732bf9
Sanyok133 <kozyuckov.alex@gmail.com>
2023-04-21 14:16:10 +0400

    feat: add ability to change circle remove speed, add leaderboard

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
 const INTERVAL_TIMEOUT = 1000;
-const CIRCLE_DISAPPEAR_TIME = 1000;
 const MIN_CIRCLE_SIZE = 10;
 const MAX_CIRCLE_SIZE = 60;
 const CIRCLE_COLORS = [
@@ -9,20 +8,35 @@ const CIRCLE_COLORS = [
   'linear-gradient(90deg, #ffe653 0%, #ffe53f 47%, #ffff00 100%)',
   'linear-gradient(90deg, #da83ff 0%, #ff24e5 47%, #ad00ff 100%)'
 ];
+const MIN_USERNAME_LENGTH = 3;
+const MAX_USERNAME_LENGTH = 20;
 
 const screens = document.querySelectorAll('.screen');
 const menuScreen = document.querySelector('#menu-screen');
-const levelChangeScreen = document.querySelector('#level-change-screen');
+const settingsScreen = document.querySelector('#level-settings-screen');
 const startBtn = document.querySelector('#start');
 const timeList = document.querySelector('#time-list');
 const timerBlock = document.querySelector('.timer');
 const timer = document.querySelector('#time');
 const board = document.querySelector('#board');
 const modeInput = document.querySelector('#no-misses');
+const speedRange = document.querySelector('.range-speed');
+const speedValue = document.querySelector('.range-speed-value');
+
+const leaderboardList = document.querySelector('.leaderboard__list');
+const nameInput = document.querySelector('#name');
 
 let time = 0;
 let score = 0;
 let missClicks = 0;
+
+let circleDisappearTime = 1000;
+speedValue.textContent = speedRange.value;
+
+speedRange.addEventListener('change', () => {
+  circleDisappearTime = speedRange.value;
+  speedValue.textContent = speedRange.value;
+})
 
 const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
@@ -66,6 +80,16 @@ const decreaseTime = () => {
   }
 };
 
+const handleMissClick = () => {
+  removeCircle();
+  createRandomCircle();
+  missClicks++;
+
+  if (modeInput.checked && missClicks > 0) {
+    finishGame();
+  }
+}
+
 let intervalId;
 let circleRemove;
 const startGame = () => {
@@ -75,11 +99,7 @@ const startGame = () => {
   missClicks = 0;
 
   createRandomCircle();
-  circleRemove = setInterval(() => {
-    removeCircle();
-    createRandomCircle();
-    missClicks++;
-  }, CIRCLE_DISAPPEAR_TIME);
+  circleRemove = setInterval(handleMissClick, circleDisappearTime);
 
   if (time === 60) {
     timer.innerText = `01:00`;
@@ -91,7 +111,32 @@ const startGame = () => {
   intervalId = setInterval(decreaseTime, INTERVAL_TIMEOUT);
 };
 
+const scores = [];
+
+const addToLeaderboard = () => {
+  leaderboardList.innerHTML = '';
+
+  scores.push({
+    name: nameInput.value,
+    points: score
+  });
+
+  const sortedScores = scores.sort((a, b) => b.points - a.points);
+
+  sortedScores.forEach((score) => {
+    const leaderboardItem = document.createElement('li');
+    leaderboardItem.classList.add('leaderboard__item');
+    leaderboardItem.innerHTML = `<span class="leaderboard__name">${score.name}</span>`
+    leaderboardList.append(leaderboardItem);
+  })
+}
+
 const finishGame = () => {
+  if (modeInput.checked && missClicks === 0) {
+    addToLeaderboard();
+    nameInput.value = '';
+  }
+
   timerBlock.classList.add('hidden');
   board.innerHTML = `<h1>Счёт: <span class="primary">${score}</span></h1>
                      <h1>Промахи: <span class="primary">${missClicks}</span></h1`;
@@ -108,29 +153,35 @@ startBtn.addEventListener('click', (evt) => {
   menuScreen.classList.add('up');
 });
 
+const isUsernameCorrect = () => nameInput.value !== '' &&
+  nameInput.value.length >= MIN_USERNAME_LENGTH &&
+  nameInput.value.length <= MAX_USERNAME_LENGTH &&
+  !scores.some(({name}) => name === nameInput.value);
+
 timeList.addEventListener('click', (evt) => {
   if (evt.target.classList.contains('time-btn')) {
-    time = parseInt(evt.target.dataset.time);
-    levelChangeScreen.classList.add('up');
-    startGame();
+    if (isUsernameCorrect()) {
+      time = parseInt(evt.target.dataset.time);
+      settingsScreen.classList.add('up');
+      startGame();
+    } else {
+      // errorMessage.classList.remove('hidden');
+
+      // setTimeout(() => {
+      //   errorMessage.classList.add('hidden');
+      // }, 3000);
+    }
   }
 });
 
 board.addEventListener('click', (evt) => {
   if (evt.target.classList.contains('circle')) {
     clearInterval(circleRemove);
-    circleRemove = setInterval(() => {
-      removeCircle();
-      createRandomCircle();
-      missClicks++;
-    }, CIRCLE_DISAPPEAR_TIME);
+    circleRemove = setInterval(handleMissClick, circleDisappearTime);
     score++;
     removeCircle();
     createRandomCircle();
   } else {
     missClicks++;
-    if (modeInput.checked) {
-      finishGame();
-    }
   }
 });
