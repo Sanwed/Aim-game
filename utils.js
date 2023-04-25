ae084fa5d77bb1baf22e6629548a28849912155e
Sanyok133 <kozyuckov.alex@gmail.com>
2023-04-25 16:48:56 +0400

    feat:: add utility functions

---
+++ utils.js
@@ -0,0 +1,7 @@
+export const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export const getRandomArrayElement = (array) => array[getRandomNumber(0, array.length - 1)];
